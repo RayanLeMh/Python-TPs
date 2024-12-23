92cf1a11704bf737679ac1f96cb317ce05d41223
RayanLeMh <rayan.mahm08@gmail.com>
2024-12-23 20:28:53 +0100

    Correction of exo 3

--- exo3.py
+++ exo3.py
@@ -1,12 +1,16 @@
-Amount = int(input("Total amount: "))
+Amount = float(input("Total amount: "))
 numItems = int(input("Number of items: "))
-DayOfWeek = str(input("Day of the week: "))
+DayOfWeek = str(input("Day of the week: ")).capitalize()
 
-if DayOfWeek == (Monday // Tuesday // Thursday // Friday):
-    Amount = Amount * 0.1
-elif DayOfWeek == (Saturday // Sunday):
-    Amount == Amount * 0.2
+if DayOfWeek in ["Monday" , "Tuesday" , "Wednesday" , "Thursday" , "Friday"]:
+    Amount = Amount - Amount * 0.1
+elif DayOfWeek in ["Saturday" , "Sunday"]:
+    Amount = Amount - Amount * 0.2
+else:
+    print("Please write the day properly")
 
 if numItems>5:
-    Amount = Amount + Amount * 0.05 
+    Amount = Amount - Amount * 0.05 
+
+print("Total price after discount: ", Amount, " dinars")
 
