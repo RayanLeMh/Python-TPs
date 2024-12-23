dbd156f958b83073b019019fe317d48ebea51dd6
RayanMHD <Rayanmahm2019@gmail.com>
2024-12-23 18:54:49 +0100

    Re-push all the exercices in a new github account, hopefully it will solve my problem

---
+++ exo3.py
@@ -0,0 +1,12 @@
+Amount = int(input("Total amount: "))
+numItems = int(input("Number of items: "))
+DayOfWeek = str(input("Day of the week: "))
+
+if DayOfWeek == (Monday // Tuesday // Thursday // Friday):
+    Amount = Amount * 0.1
+elif DayOfWeek == (Saturday // Sunday):
+    Amount == Amount * 0.2
+
+if numItems>5:
+    Amount = Amount + Amount * 0.05 
+
